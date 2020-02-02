162c8a30181ee1548fb0a5e2e72ef76292efe4b8
Zoe Tsekas <zoetsekas@live.co.uk>
2020-02-02 19:20:23 +0000

    default favorites removed

--- vagrant/install-desktop.sh
+++ vagrant/install-desktop.sh
@@ -3,27 +3,18 @@ set -ex
 
 export DEBIAN_FRONTEND=noninteractive
 
-dconf write /org/gnome/shell/favorite-apps "['firefox.desktop', 'Anaconda.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'PyCharm.desktop', 'AndroidStudio.desktop']"
-
 # Set password for default "ubuntu" user
 echo "ubuntu:ubuntu" | sudo chpasswd
 
-# https://askubuntu.com/questions/1067929/on-18-04-package-virtualbox-guest-utils-does-not-exist
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-add-repository universe
 sudo apt-add-repository multiverse
 sudo apt-get install ubuntu-desktop -y
 
-# Install xfce and virtualbox additions.
+# dconf write /org/gnome/shell/favorite-apps "['firefox.desktop', 'Anaconda.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'PyCharm.desktop', 'AndroidStudio.desktop']"
 
 sudo apt-get install -y virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
-
-
-
-# Permit anyone to start the GUI
-# TODO - needed?
-#sudo sed -i 's/allowed_users=.*$/allowed_users=anybody/' /etc/X11/Xwrapper.config
 
 # Use English keyboard layout
 #L='en' && sudo sed -i 's/XKBLAYOUT=\"\w*"/XKBLAYOUT=\"'$L'\"/g' /etc/default/keyboard
